e9735682197f0ce70b33899aa8493b0f0ca28ecb
OGATA Katsuhiro <ogwata@vivliostyle.org>
2023-07-27 21:07:48 +0900

    add files

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,31 +1,42 @@
 module.exports = {
-  title: 'miura', // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
-  author: 'ogwata@vivliostyle.org <ogwata@vivliostyle.org>', // default to `author` in `package.json` or undefined.
-  // language: 'ja', // default to undefined.
-  // size: 'A4', // paper size.
+  title: '實録三浦党', // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
+  author: '岩間尹', // default to `author` in `package.json` or undefined.
+  language: 'ja', // default to undefined.
+  size: 'A5', // paper size.
   theme: '@vivliostyle/theme-bunko@^0.5.1', // .css or local dir or npm package. default to undefined.
   entry: [
-    'manuscript.md', // `title` is automatically guessed from the file (frontmatter > first heading).
-    // {
-    //   path: 'epigraph.md',
-    //   title: 'Epigraph', // title can be overwritten (entry > file),
-    //   theme: '@vivliostyle/theme-whatever', // theme can be set individually. default to the root `theme`.
-    // },
-    // 'glossary.html', // html can be passed.
-  ], // `entry` can be `string` or `object` if there's only single markdown file.
-  // entryContext: './manuscripts', // default to '.' (relative to `vivliostyle.config.js`).
-  // output: [ // path to generate draft file(s). default to '{title}.pdf'
-  //   './output.pdf', // the output format will be inferred from the name.
-  //   {
-  //     path: './book',
-  //     format: 'webpub',
-  //   },
-  // ],
-  // workspaceDir: '.vivliostyle', // directory which is saved intermediate files.
-  // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
-  // cover: './cover.png', // cover image. default to undefined.
-  // vfm: { // options of VFM processor
-  //   hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
-  //   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
-  // },
+    'text/0.md',
+    'text/1-1.md',
+    'text/1-2.md',
+    'text/1-3.md',
+    'text/1-4.md',
+    'text/1-5.md',
+    'text/1-6.md',
+    'text/1-7.md',
+    'text/1-8.md',
+    'text/1-9.md',
+    'text/2-1.md',
+    'text/2-2.md',
+    'text/3-1.md',
+    'text/3-2.md',
+    'text/4-1.md',
+    'text/4-2.md',
+    'text/4-3.md',
+    'text/4-4.md',
+    'text/4-5.md',
+    'text/5.md',
+    'text/6.md',
+    'text/7-1.md',
+    'text/7-2.md',
+    'text/7-3.md',
+    'text/7-4.md',
+    'text/7-5.md',
+    'text/7-6.md',
+    'text/7-7.md',
+    'text/7-8.md',
+    'text/9.md',
+  ],
+  http: true,
+  timeout: 1000000,
+　
 }
